02cfb8ec7102bb277533818ff1b216a93e5dc4e7
Patrick Tescher <pat2man@gmail.com>
2018-03-29 12:49:28 -0700

    Fix for metamask

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import Game from './Game.js';
 
 import { Bitski } from 'bitski';
 
-import { Web3 } from 'web3';
+import Web3 from 'web3';
 
 window.addEventListener('load', function () {
   let url = new URL(window.location.href);
@@ -17,25 +17,25 @@ window.addEventListener('load', function () {
 
   if (web3) {
     web3 = new Web3(web3.currentProvider)
-    loadData(web3);
+    window.web3 = web3;
+    showApp(web3);
   } else {
 
     web3 = bitski.getWeb3();
     window.web3 = web3;
 
     bitski.getSignedInUser().then(function (user) {
-      showApp(bitski);
+      showApp(bitski.getWeb3());
     }).catch(function (error) {
       showLoginButton(bitski);
     });
   }
 })
 
-function showApp(bitski) {
+function showApp(web3) {
   document.getElementById('signed-out').style.display = 'none';
   document.getElementById('signed-in').style.display = 'block';
   window.game = new Game(document.getElementById('game'), web3);
-  loadData(bitski.getWeb3());
 }
 
 function showLoginButton(bitski) {
@@ -49,29 +49,7 @@ function showLoginButton(bitski) {
     }
 
     if (user) {
-      showApp(bitski);
+      showApp(bitski.getWeb3());
     }
   }
-}
-
-function loadData(web3) {
-  web3.eth.getAccounts(function (error, accounts) {
-    if (error) {
-      document.getElementById('wallet').innerText = 'Error: ' + error
-      document.getElementById('balance').innerText = ''
-    } else if (accounts.length == 0) {
-      document.getElementById('wallet').innerText = 'Error: no accounts'
-      document.getElementById('balance').innerText = ''
-    } else {
-      web3.eth.getBalance(accounts[0], function (error, result) {
-        if (error) {
-          document.getElementById('wallet').innerText = 'Error: ' + error
-          document.getElementById('balance').innerText = ''
-        } else {
-          document.getElementById('wallet').innerText = 'Wallet: ' + accounts[0]
-          document.getElementById('balance').innerText = 'Balance: ' + result
-        }
-      })
-    }
-  })
 }
